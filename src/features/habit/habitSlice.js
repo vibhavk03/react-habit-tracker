2592a1e09f7b9ba91e0fee2d3187fa83ec065c9e
vibhavk03 <vibhavkashyap3@gmail.com>
2023-09-16 18:47:48 +0530

    adding weekLog to initial habit state

--- src/features/habit/habitSlice.js
+++ src/features/habit/habitSlice.js
@@ -1,5 +1,24 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
 
+const dayMapper = {
+  0: 'Sunday',
+  1: 'Monday',
+  2: 'Tuesday',
+  3: 'Wednesday',
+  4: 'Thursday',
+  5: 'Friday',
+  6: 'Saturday',
+};
+const getPreviousDate = (numOfDays) => {
+  const today = new Date();
+  const currDate = new Date(today.getTime() - numOfDays * 24 * 60 * 60 * 1000);
+  const dd = currDate.getDate();
+  const mm = currDate.getMonth();
+  const yy = currDate.getFullYear();
+  const day = dayMapper[currDate.getDay()];
+  return `${day}:${dd}-${mm}-${yy}`;
+};
+
 const initialState = {
   habits: [],
 };
@@ -12,7 +31,50 @@ export const habitSlice = createSlice({
       const newHabit = {
         id: nanoid(),
         text: action.payload,
-        completed: false,
+        weekLog: [
+          {
+            id: 0,
+            day: getPreviousDate(0).split(':')[0],
+            date: getPreviousDate(0).split(':')[1],
+            isCompleted: null,
+          },
+          {
+            id: 1,
+            day: getPreviousDate(1).split(':')[0],
+            date: getPreviousDate(1).split(':')[1],
+            isCompleted: null,
+          },
+          {
+            id: 2,
+            day: getPreviousDate(2).split(':')[0],
+            date: getPreviousDate(2).split(':')[1],
+            isCompleted: null,
+          },
+          {
+            id: 3,
+            day: getPreviousDate(3).split(':')[0],
+            date: getPreviousDate(3).split(':')[1],
+            isCompleted: null,
+          },
+          {
+            id: 4,
+            day: getPreviousDate(4).split(':')[0],
+            date: getPreviousDate(4).split(':')[1],
+            isCompleted: null,
+          },
+          {
+            id: 5,
+            day: getPreviousDate(5).split(':')[0],
+            date: getPreviousDate(5).split(':')[1],
+            isCompleted: null,
+          },
+          {
+            id: 6,
+            day: getPreviousDate(6).split(':')[0],
+            date: getPreviousDate(6).split(':')[1],
+            isCompleted: null,
+          },
+        ],
       };
       state.habits.push(newHabit);
     },
