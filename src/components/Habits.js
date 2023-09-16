7d4c221556b15eaf096e9cb387021593ba4a4490
vibhavk03 <vibhavkashyap3@gmail.com>
2023-09-16 20:12:02 +0530

    displaying past week completed count

--- src/components/Habits.js
+++ src/components/Habits.js
@@ -6,6 +6,21 @@ const Habits = () => {
   const habits = useSelector((state) => state.habits);
   const dispatch = useDispatch();
 
+  const getCompletedCount = (habitId) => {
+    // get for how many past days this habit is completed
+    let count = 0;
+    habits.forEach((habit) => {
+      if (habit.id === habitId) {
+        habit.weekLog.forEach((day) => {
+          if (day.isCompleted === true) {
+            count++;
+          }
+        });
+      }
+    });
+    return count;
+  };
+
   return (
     <div className="habits-container flex mg-top-bottom-5vh">
       <div className="limit-width-55 flex habits-list">
@@ -13,7 +28,7 @@ const Habits = () => {
           <div key={habit.id} className="individual-habit flex">
             <div className="inner-habit-left">
               <div className="habit-text">{habit.text}</div>
-              <div>7/7 for past week</div>
+              <div>{getCompletedCount(habit.id)}/7 for past week</div>
             </div>
             <div className="inner-habit-center"></div>
             <div className="inner-habit-right flex">
